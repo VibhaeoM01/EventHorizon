ed4b9b52078616fc88019537502f9476f22c0c68
Vibhaeo Mudia <VibhaeoM01>
2025-03-24 21:11:39 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,21 @@
+var tl = gsap.timeline();
+
+tl.from(".nav",
+  {
+    y:-20,
+    opacity:0,
+    duration:1,
+    // delay:0.5,
+  }
+)
+
+tl.from(".hero-content",
+  {
+    x:-60,
+    opacity:0,
+    duration:1,
+
+    
+  }
+)
+
